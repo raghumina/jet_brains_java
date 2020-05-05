edb354803a377aafcfca05c4ed58f9a7cb046f0d
hazard00 <42498911+hazard00@users.noreply.github.com>
2020-05-05 00:33:40 -0700

    java switch multifile
    java switch

---
+++ java_switch/src/com/company/SwitchCase.java
@@ -0,0 +1,19 @@
+package com.company;
+
+//import com.company.
+class SwitchCase {
+
+    public  void problem1(String userInput){
+//        Main.
+        System.out.println("hello world problem1");
+        return ;
+    }
+    public  void problem2(String userInput){
+//        Main.
+        System.out.println("hello world  problem2");
+        return ;
+    }
+
+   // public void problem1() {
+   // }
+}

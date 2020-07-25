49cc26157354436cf6120c20bfe818833f731fed
raghu <raghumina11@gmail.com>
2020-07-25 21:51:50 +0530

    Create file1.java

---
+++ java_switch/src/com/company/file1.java
@@ -0,0 +1,14 @@
+package com.company
+
+public static void main(string[] args)
+        {
+            byte a=30,b=12,c;
+
+            c=(byte)(a+b);
+            c=(byte)(b*0);
+            float f=10.91f;
+            int i=(int)f;
+            system.out.println(c);
+            system.out.println(i);
+
+        }
